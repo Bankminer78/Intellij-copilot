975853c530d20fc1d86a87d0a0bb24c04b5ad9a7
GitHub Action <action@github.com>
2025-05-31 14:44:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/bankminer78/intellijcopilot/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/bankminer78/intellijcopilot/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.bankminer78.intellijcopilot.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
